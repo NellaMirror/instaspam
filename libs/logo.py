970cff8f3ef9299ad7c7668c12a1946ad01683c7
Nella97 <38042163+Nella97@users.noreply.github.com>
2021-08-19 22:45:14 +0200

    Update logo.py

--- libs/logo.py
+++ libs/logo.py
@@ -16,7 +16,5 @@ logo = """
 
 def print_logo():
     print(Fore.RED + Style.BRIGHT + logo + Style.RESET_ALL + Style.BRIGHT +"\n")
-    print(Fore.MAGENTA + "      Code by N3ll4"+ Style.RESET_ALL + Style.BRIGHT)
-    print(Fore.CYAN + "\n", "-> a big thank you goes to my community")
-    print ("\n", "-> Greetings go out to error 404, R4V3N, Sonix, Sn0wbl0wer, bashpr0mt:\n    " + choice(youtube_urls))
-    print(Style.RESET_ALL + Style.BRIGHT, Style.BRIGHT)
+    print(Fore.MAGENTA + "Code by N3ll4"+ Style.RESET_ALL + Style.BRIGHT)
+    print(Fore.CYAN + "\n", "-> a big thank you goes to my Community")
